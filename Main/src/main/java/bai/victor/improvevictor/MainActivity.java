6409355661959fb7382166bf77c92ac12c31a1ae
tinyWhite <1124893581@qq.com>
2017-07-14 17:11:47 +0800

    添加从 H5 链接跳转进App的逻辑

--- Main/src/main/java/bai/victor/improvevictor/MainActivity.java
+++ Main/src/main/java/bai/victor/improvevictor/MainActivity.java
@@ -3,6 +3,7 @@ package bai.victor.improvevictor;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import bai.victor.improvevictor.ui.activity.BaseActivity;
 import bai.victor.improvevictor.ui.activity.SwipeMenuActivity;
@@ -12,10 +13,27 @@ import bai.victor.improvevictor.ui.activity.SwipeMenuActivity;
  */
 
 public class MainActivity extends BaseActivity{
+    private TextView mJumpInfo;
+    String url;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // 接收从 H5 链接跳转的Intent
+        Intent intent = getIntent();
+        String data = intent.getDataString();
+        if (data != null){
+            String[] split = data.split("data/");
+            url = split.length > 0 ? split[1] : "no data";
+        }
+        initView();
+    }
+
+    private void initView() {
+        mJumpInfo = (TextView) findViewById(R.id.jump);
+        if (url != null){
+            mJumpInfo.setText(url);
+        }
     }
 
     public void onClick(View view){
