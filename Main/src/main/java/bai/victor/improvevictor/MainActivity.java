e3b05b60e2b8024a16f85b4f11788d88aad5dddb
tinyWhite <1124893581@qq.com>
2017-07-18 17:24:08 +0800

    优化H5跳转的信息获取方式，添加主界面左侧菜单

--- Main/src/main/java/bai/victor/improvevictor/MainActivity.java
+++ Main/src/main/java/bai/victor/improvevictor/MainActivity.java
@@ -1,18 +1,28 @@
 package bai.victor.improvevictor;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
 
 import bai.victor.improvevictor.ui.activity.BaseActivity;
 import bai.victor.improvevictor.ui.activity.SwipeMenuActivity;
+import bai.victor.utils.LogUtils;
+
+import static android.R.attr.data;
 
 /**
  * Created by Victor-Bai on 2017/7/13.
  */
 
 public class MainActivity extends BaseActivity{
+    private long lastBackPressedTime;
+
+    private NavigationView navigationView;
+
     private TextView mJumpInfo;
     String url;
     @Override
@@ -21,23 +31,46 @@ public class MainActivity extends BaseActivity{
         setContentView(R.layout.activity_main);
         // 接收从 H5 链接跳转的Intent
         Intent intent = getIntent();
+        String action = intent.getAction();
         String data = intent.getDataString();
-        if (data != null){
-            String[] split = data.split("data/");
-            url = split.length > 0 ? split[1] : "no data";
+        if (Intent.ACTION_VIEW.equals(action)){
+            Uri uri = intent.getData();
+            if (uri != null){
+                String host = uri.getHost();
+                String id = uri.getQueryParameter("id");
+                String name = uri.getQueryParameter("name");
+                String path = uri.getPath();
+                url = "跳转的链接 \n"+"url:"+data + "\n host:"+host+"\n id:"+id+"\n name:"+name+"\n path:"+path;
+            }
         }
         initView();
     }
 
     private void initView() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        initToolBar(toolbar, getString(R.string.app_name),false);
+
         mJumpInfo = (TextView) findViewById(R.id.jump);
         if (url != null){
             mJumpInfo.setText(url);
         }
+
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
     }
 
     public void onClick(View view){
         Intent intent = new Intent(this, SwipeMenuActivity.class);
         startActivity(intent);
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (lastBackPressedTime + 2000 >= System.currentTimeMillis()){
+            super.onBackPressed();
+        }else {
+            toast("再点一次退出应用！");
+        }
+        lastBackPressedTime = System.currentTimeMillis();
     }
 }
--- Main/src/main/java/bai/victor/improvevictor/ui/activity/BaseActivity.java
+++ Main/src/main/java/bai/victor/improvevictor/ui/activity/BaseActivity.java
@@ -3,13 +3,19 @@ package bai.victor.improvevictor.ui.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.widget.Toast;
+
+import bai.victor.improvevictor.R;
 
 /**
  * Created by Victor-Bai on 2017/7/10.
  */
 
 public class BaseActivity extends AppCompatActivity{
+    private boolean isShowRight;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -26,6 +32,25 @@ public class BaseActivity extends AppCompatActivity{
     }
 
     protected void initEvents() {
+    }
+
+    public void initToolBar(Toolbar toolbar, String title, boolean showHomeAsUp){
+        initToolBar(toolbar, title, showHomeAsUp, false);
+    }
+
+    public void initToolBar(Toolbar toolbar, String title, boolean showHomeAsUp, boolean isShowRight){
+        this.isShowRight = isShowRight;
+        toolbar.setTitle(title);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(showHomeAsUp);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if (isShowRight){
+            getMenuInflater().inflate(R.menu.toolbar_right, menu);
+        }
+        return super.onCreateOptionsMenu(menu);
     }
 
     public void toast(String str){
